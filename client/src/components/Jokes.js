e573c4604b99f87f6461041e50bc765d6487ac51
DirupT <iamcooled@gmail.com>
2018-08-10 11:17:41 -0500

    added redirection page if user isnt logged in

--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import axios from 'axios';
 import Joke from './Joke';
+import Timeout from './Timeout/Timeout';
 
 class Jokes extends React.Component {
     constructor() {
         super();
 
         this.state = {
-            jokes: []
+            loggedIn: true,
+            jokes: [],
+            time: 5
         }
     }
 
@@ -18,16 +21,44 @@ class Jokes extends React.Component {
                 .get('http://localhost:5000/api/jokes', { headers: { Authorization: token } })
                 .then(response => this.setState({ jokes: response.data }))
                 .catch(err => console.log(err.response));
+        } else {
+            this.setState({ loggedIn: false });
+            this.setInterval();
+            this.setTimeout();
         }
     }
 
+    logout = () => {
+        localStorage.removeItem('token');
+        window.location.reload();
+    }
+
+    setTimeout = () => {
+        this.props.setTimeout(() => {
+            this.props.history.push('/signin');
+        }, this.state.time * 1000);
+    }
+
+    setInterval = () => {
+        let timeLeft = this.state.time;
+        this.props.setInterval(() => {
+            timeLeft--;
+            this.setState({ time: timeLeft });
+        }, 1000)
+    }
+
     render() {
+        if (!this.state.loggedIn) {
+            return <h3>You need to be logged in to view this page... Redirecting to login page in {this.state.time}</h3>
+        }
+
         return (
             <div>
+                <button onClick={this.logout}>Logout</button>
                 {this.state.jokes.map((joke, index) => <Joke key={index} joke={joke} />)}
             </div>
         );
     }
 }
 
-export default Jokes;
+export default Timeout(Jokes);
