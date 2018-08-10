1fc95e90b57077d39f57601ed09dec9d232a5f90
DirupT <iamcooled@gmail.com>
2018-08-10 11:47:06 -0500

    updated styling of signin component

---
+++ client/src/components/ReusableComponents/Login.js
@@ -0,0 +1,107 @@
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import { Button, Form, FormGroup, Input } from 'reactstrap';
+
+export const LoginContainer = styled.div`
+    max-width: 100%;
+    width: 100vw;
+    height: 50vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const LoginForm = styled(Form)`
+    max-width: 350px;
+    width: 100%;
+    padding: 22px 40px;
+    border: 1px solid #e6e6e6;
+`
+
+export const WordContainer = styled(FormGroup)`
+    display: flex;
+    justify-content: center;
+`
+
+export const Word = styled.h1`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 175px;
+    height: 51px;
+`
+
+export const ForgotPassword = styled.a`
+    display: flex;
+    justify-content: center;
+    font-size: 12px;
+    line-height: 14px;
+    margin-top: 25px;
+    color: #003569;
+    &:hover {
+        text-decoration: none;
+        color: #003569;
+    }  
+`
+
+export const ForgotForm = styled(FormGroup)`
+    margin: 0;
+`
+
+export const BottomLoginContent = styled.div`
+    max-width: 350px;
+    width: 100%;
+    padding: 8px 40px;
+    border: 1px solid #e6e6e6;
+    margin-top: 10px
+`
+
+export const BottomText = styled.div`
+    font-size: 14px;
+    color: #262626;
+    margin: 15px;
+    text-align: center;
+`
+
+export const SignUp = styled(Link)`
+    color: #3897f0;
+    &:hover {
+        text-decoration: none;
+        color: #3897f0;
+    }
+`
+
+export const StyledFormControl = styled(Input)`
+    font-size: 12px;
+    background: #fafafa;
+    border: 1px solid #efefef;
+    color: #999;
+    height: 36px;
+    line-height: 36px;
+    &::placeholder {
+        color: #999;
+    }
+`
+
+export const StyledButton = styled(Button)`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    max-height: 28px;
+    background: #3897F0;
+    border-color: #3898F0;
+    border-radius: 3px;
+    border-style: solid;
+    font-size: 14px;
+    font-weight: 600;
+    line-height: 26px;
+    border-width: 1px;
+    padding: 0;
+    color: #FFF;
+    &:hover {
+        background: #3897F0;
+        border-color: #3898F0;
+    }
+`
--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import axios from 'axios';
+import { FormGroup } from 'reactstrap';
+import {
+    LoginContainer, LoginForm, WordContainer, Word, ForgotPassword,
+    ForgotForm, BottomLoginContent, BottomText, SignUp, StyledFormControl, StyledButton
+} from './ReusableComponents/Login';
 
 class SignIn extends React.Component {
     constructor() {
@@ -34,11 +39,39 @@ class SignIn extends React.Component {
 
     render() {
         return (
-            <form onSubmit={event => event.preventDefault()}>
-                <input placeholder='Username' name='username' value={this.state.username} onChange={this.handleInput} type='text' />
-                <input placeholder='Password' name='password' value={this.state.password} onChange={this.handleInput} type='password' />
-                <button onClick={this.login}>Log in</button>
-            </form>
+            <LoginContainer>
+
+                <LoginForm onSubmit={event => event.preventDefault()}>
+
+                    <WordContainer>
+                        <Word>Sign In</Word>
+                    </WordContainer>
+
+                    <FormGroup>
+                        <StyledFormControl value={this.state.username} onChange={this.handleInput} type="username" name="username" placeholder="Username" />
+                    </FormGroup>
+
+                    <FormGroup>
+                        <StyledFormControl value={this.state.password} onChange={this.handleInput} type="password" name="password" placeholder="Password" />
+                    </FormGroup>
+
+                    <StyledButton type='submit' style={this.state.username.length > 0 || this.state.password.length > 0 ? { background: '#3897F0' } : { opacity: .3 }} onClick={this.login}>Log In</StyledButton>
+
+                    <ForgotForm>
+                        <ForgotPassword href='#_'>Forgot password?</ForgotPassword>
+                    </ForgotForm>
+
+                </LoginForm>
+
+                <BottomLoginContent>
+
+                    <BottomText>
+                        Don't have an account? <SignUp to='/signup'>Sign up</SignUp>
+                    </BottomText>
+
+                </BottomLoginContent>
+
+            </LoginContainer>
         );
     }
 }
